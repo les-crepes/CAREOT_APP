3b794c751cfc26270b6807c48e1e6ecfc6cbcb33
Luca Coduri <coduri.vitor@gmail.com>
2022-08-23 14:57:10 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,

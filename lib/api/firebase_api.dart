bf1bed5863a0b56e65e250f1d671223ae656c3e6
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-08-24 09:03:59 +0200

    commented unused imports

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,8 +1,9 @@
 import 'api.dart';
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_core/firebase_core.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import '../firebase_options.dart';
 
 class FirebaseApi implements Api {
   @override

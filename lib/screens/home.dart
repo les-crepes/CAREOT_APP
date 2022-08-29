18429fd2ebcf086c57462f8c628118a36c616edd
Chloé <cfontaine@citycable.ch>
2022-08-29 16:31:15 +0200

    Add nav bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:pdg_app/router/router.gr.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -21,15 +22,29 @@ class HomeScreen extends StatelessWidget {
             opacity: animation,
             child: child,
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: tabsRouter.activeIndex,
-            onTap: (index) {
-              tabsRouter.setActiveIndex(index);
-            },
-            items: const [
-              BottomNavigationBarItem(label: "chat", icon: Icon(Icons.chat)),
-              BottomNavigationBarItem(
-                  label: "diary", icon: Icon(Icons.calendar_month)),
+          bottomNavigationBar: GNav(
+            rippleColor: Theme.of(context).colorScheme.secondary,
+            hoverColor: Theme.of(context).colorScheme.secondary,
+            color: Theme.of(context).colorScheme.onSecondary,
+            activeColor: Theme.of(context).colorScheme.onSecondary,
+            duration: const Duration(milliseconds: 900),
+            tabBackgroundColor: Theme.of(context).colorScheme.secondary,
+            gap: 8,
+            padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 12),
+            tabMargin: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            tabs: const [
+              GButton(
+                icon: Icons.chat_bubble_outline,
+                text: "Chat",
+              ),
+              GButton(
+                icon: Icons.calendar_month_outlined,
+                text: "Diary",
+              ),
+              GButton(
+                icon: Icons.account_circle_outlined,
+                text: "Profile",
+              )
             ],
           ),
         );

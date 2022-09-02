2f8d5cb66877471b4ec897ba573b2a0b637f0bb2
Chloé <cfontaine@citycable.ch>
2022-09-02 11:55:05 +0200

    Fix

--- lib/screens/update_client_record.dart
+++ lib/screens/update_client_record.dart
@@ -31,43 +31,39 @@ class UpdateClientRecord extends StatelessWidget {
       children: [
         StraightTopBar(title: "$_clientFirstName's record"),
         Expanded(
-          child: ListView(
-            padding: const EdgeInsets.all(16.0),
-            children: [
-              MainTextField(
-                  name: "BMI",
-                  icon: Icon(Icons.query_stats_outlined),
-                  keyboardType: TextInputType.number),
-              const SizedBox(height: 15.0),
-              MainTextField(
-                  name: "Weight",
-                  icon: Icon(Icons.monitor_weight_outlined),
-                  keyboardType: TextInputType.number),
-              const SizedBox(height: 15.0),
-              MainTextField(
-                  name: "Diagnosic", icon: Icon(Icons.recommend_outlined)),
-              const SizedBox(height: 15.0),
-              MainTextField(
-                  name: "Comments", icon: Icon(Icons.comment_outlined)),
-              const SizedBox(height: 15.0),
-              MainTextField(name: "Motivations", icon: Icon(Icons.favorite)),
-              const SizedBox(height: 15.0),
-              MainTextField(
-                  name: "Food objectives",
-                  icon: Icon(Icons.food_bank_outlined)),
-              const SizedBox(height: 15.0),
-              MainTextField(
-                name: "Start date",
-                icon: Icon(Icons.date_range),
-                keyboardType: TextInputType.datetime,
-              ),
-              const SizedBox(height: 15.0),
-              MainTextField(
-                name: "End date",
-                icon: Icon(Icons.date_range),
-                keyboardType: TextInputType.datetime,
-              ),
-          ),
+          child: ListView(padding: const EdgeInsets.all(16.0), children: [
+            MainTextField(
+                name: "BMI",
+                icon: Icon(Icons.query_stats_outlined),
+                keyboardType: TextInputType.number),
+            const SizedBox(height: 15.0),
+            MainTextField(
+                name: "Weight",
+                icon: Icon(Icons.monitor_weight_outlined),
+                keyboardType: TextInputType.number),
+            const SizedBox(height: 15.0),
+            MainTextField(
+                name: "Diagnosic", icon: Icon(Icons.recommend_outlined)),
+            const SizedBox(height: 15.0),
+            MainTextField(name: "Comments", icon: Icon(Icons.comment_outlined)),
+            const SizedBox(height: 15.0),
+            MainTextField(name: "Motivations", icon: Icon(Icons.favorite)),
+            const SizedBox(height: 15.0),
+            MainTextField(
+                name: "Food objectives", icon: Icon(Icons.food_bank_outlined)),
+            const SizedBox(height: 15.0),
+            MainTextField(
+              name: "Start date",
+              icon: Icon(Icons.date_range),
+              keyboardType: TextInputType.datetime,
+            ),
+            const SizedBox(height: 15.0),
+            MainTextField(
+              name: "End date",
+              icon: Icon(Icons.date_range),
+              keyboardType: TextInputType.datetime,
+            ),
+          ]),
         ),
       ],
     );

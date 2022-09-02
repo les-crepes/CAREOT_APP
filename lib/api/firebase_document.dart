fe38f84d6ddc9d2fab7e680cd3da39cbf289d5d4
Nelson-Jnrnd <79466777+Nelson-Jnrnd@users.noreply.github.com>
2022-09-02 11:16:13 +0200

    new file upload

--- lib/api/firebase_document.dart
+++ lib/api/firebase_document.dart
@@ -3,39 +3,23 @@ import 'dart:io';
 import 'package:flutter/widgets.dart';
 import 'package:pdg_app/api/ifile.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
 class FirebaseFile implements IFile {
   final bucket = FirebaseStorage.instance;
   late final storageRef = bucket.ref();
 
+  /// Uploads the file to the firebase storage under a specific path. with a unique name.
   @override
-  Future<void> uploadFile(File file, int type) async {
-    String filePath = (type == 0) ? 'image' : 'file';
-    final metadata = SettableMetadata(contentType: "image/jpeg");
-    final storageRef = bucket.ref();
-    final uploadTask = storageRef.child(filePath).putFile(file, metadata);
-
-    uploadTask.snapshotEvents.listen((TaskSnapshot taskSnapshot) {
-      switch (taskSnapshot.state) {
-        case TaskState.running:
-          final progress =
-              100.0 * (taskSnapshot.bytesTransferred / taskSnapshot.totalBytes);
-          log("Upload is $progress% complete.");
-          break;
-        case TaskState.paused:
-          log("Upload is paused.");
-          break;
-        case TaskState.canceled:
-          log("Upload was canceled");
-          break;
-        case TaskState.error:
-          ErrorDescription(" unsuccessful uploads");
-          break;
-        case TaskState.success:
-          log("tata !");
-          break;
-      }
-    });
+  Future<void> uploadFile(String filepath) async {
+    File file = File(filepath);
+    final fileRef = storageRef.child("file/${const Uuid().v1()}");
+    try {
+      await fileRef.putFile(file);
+    } on FirebaseException catch (e) {
+      log("Failed to upload file: $e");
+      throw Exception(e);
+    }
   }
 
   @override
--- lib/api/ifile.dart
+++ lib/api/ifile.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 abstract class IFile {
-  void uploadFile(File file, int type);
+  void uploadFile(String filepath);
   void downloadFile(String fileUrl);
   void updateFile(String fileName);
   void deleteFile(String fileId);

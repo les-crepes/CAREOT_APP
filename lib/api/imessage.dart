3eb10acc663c41b768812cfad2dda5d572d94809
Luca <coduri.vitor@gmail.com>
2022-09-04 15:32:44 +0200

    WIP

--- lib/api/imessage.dart
+++ lib/api/imessage.dart
@@ -2,8 +2,8 @@ import 'package:pdg_app/model/message.dart';
 
 abstract class IMessage {
   void createMessage(Message message);
-  Future<dynamic> readMessage(String messageId);
-  Future<List<dynamic>?> readConversation(String firstId, String secondId);
+  Future<Message> readMessage(String messageId);
+  Future<List<Message>?> readConversation(String firstId, String secondId);
   void updateMessage(Message message);
   void deleteMessage(String messageId);
 }
--- lib/provider/chat_provider.dart
+++ lib/provider/chat_provider.dart
@@ -1,28 +1,51 @@
 import 'dart:collection';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:pdg_app/api/firebase_message.dart';
 
+import '../api/firebase_user.dart';
+import '../api/imessage.dart';
+import '../api/iuser.dart';
 import '../model/message.dart';
+import '../model/user.dart';
 import 'auth_provider.dart';
 
 typedef Uid = String;
 
 class ChatProvider extends ChangeNotifier {
   /// Getting the instance of the AuthProvider class.
-  final authProvider = GetIt.I.get<AuthProvider>();
+  final AuthProvider _auth;
+  final IMessage _messageApi = FirebaseMessage(FirebaseFirestore.instance);
+  final IUser _userApi = FirebaseUser(FirebaseFirestore.instance);
 
   /// The list of messages using a [LinkedHashMap] to keep the order of the messages.
   final SplayTreeSet<Message> _messages = SplayTreeSet<Message>(
     (Message a, Message b) => a.time.compareTo(b.time),
   );
 
+  ChatProvider(AuthProvider authProvider) : _auth = authProvider;
+
   /// Returning an unmodifiable list view of the messages.
   get messages => UnmodifiableListView(_messages);
 
-  /// `fetchAllMessages()` is an asynchronous function that returns a `Future<void>` and does not take
-  /// any arguments
-  Future<void> fetchAllMessages() async {}
+  /// Fetching all the messages from the database.
+  Future<void> fetchAllMessages() async {
+    List<User> users = [];
 
-  ChatProvider();
+    if (_auth.isAdmin) {
+      final clients = await _userApi.getDietitianClient(_auth.userUid);
+      users.addAll(clients);
+    } else {
+      final diet = await _userApi.readDietitianOfClient(_auth.userUid);
+      if (diet != null) {
+        users.add(diet);
+      }
+    }
+
+    for (final user in users) {
+      _messageApi.readConversation(_auth.userUid, user.uid);
+    }
+  }
 }

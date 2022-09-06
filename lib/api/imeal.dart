709ac5fac30ce7a3ac963188d4bf363ef36d2faa
Nelson-Jnrnd <79466777+Nelson-Jnrnd@users.noreply.github.com>
2022-09-06 11:04:26 +0200

    comments imeal

--- lib/api/imeal.dart
+++ lib/api/imeal.dart
@@ -1,10 +1,17 @@
 import 'package:pdg_app/model/meal.dart';
 
+/// Interface for the meal API
 abstract class IMeal {
+  /// Add [meal] to the database
   Future<void> createMeal(Meal meal);
+  /// Returns the meal in the database with [mealId]
   Future<Meal> readMeal(String mealId);
+  /// Update the [meal] in the database
   Future<void> updateMeal(Meal meal);
+  /// Delete the meal with [mealId] from the database
   void deleteMeal(String mealId);
+  /// Get the meals of the user [userId] for the day [day]
   Future<List<Meal>> getUsersMealForDay(String userId, DateTime day);
+  /// Get the meals of the user [userId]
   Future<List<Meal>> getUserMeal(String userId);
 }

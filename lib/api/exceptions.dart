fbdb8e956dc29ba455b84f2da187ce4222943939
Nelson-Jnrnd <79466777+Nelson-Jnrnd@users.noreply.github.com>
2022-09-06 18:00:06 +0200

    authentification exception + comments

--- lib/api/exceptions.dart
+++ lib/api/exceptions.dart
@@ -1,27 +1,36 @@
-enum LoginExceptionType {
+enum AuthExceptionType {
   invalidEmail,
   userDisabled,
   userNotFound,
   wrongPassword,
+  weakPassword,
+  operationNotAllowed,
+  emailAlreadyInUse,
   unknown,
 }
 
 /// Generic exception related to Authentication. Check the error message for more details.
 class AuthenticationException implements Exception {
-  LoginExceptionType exceptionType;
+  AuthExceptionType exceptionType;
   AuthenticationException(this.exceptionType);
 
   @override
   String toString() {
     switch (exceptionType) {
-      case LoginExceptionType.invalidEmail:
+      case AuthExceptionType.invalidEmail:
         return "Invalid email";
-      case LoginExceptionType.userDisabled:
+      case AuthExceptionType.userDisabled:
         return "User disabled";
-      case LoginExceptionType.userNotFound:
+      case AuthExceptionType.userNotFound:
         return "User not found";
-      case LoginExceptionType.wrongPassword:
+      case AuthExceptionType.wrongPassword:
         return "Wrong password";
+      case AuthExceptionType.weakPassword:
+        return "Weak password";
+      case AuthExceptionType.operationNotAllowed:
+        return "Operation not allowed";
+      case AuthExceptionType.emailAlreadyInUse:
+        return "Email already in use";
       default:
         return "Unknown error";
     }
